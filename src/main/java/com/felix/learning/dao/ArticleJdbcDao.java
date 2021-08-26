2739df0efc8c8a20211d7baf6f3ed5cdc8706527
dvlp007 <1054962773@qq.com>
2021-08-26 22:59:13 +0800

    多数据源：分布式事务问题

--- src/main/java/com/felix/learning/dao/ArticleJdbcDao.java
+++ src/main/java/com/felix/learning/dao/ArticleJdbcDao.java
@@ -5,48 +5,44 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @Repository
 public class ArticleJdbcDao {
 
-    @Resource
-    private JdbcTemplate jdbcTemplate;
-
-    public void save(Article article) {
+    public void save(Article article, JdbcTemplate jdbcTemplate) {
         // 插入操作：使用update方法
         String sql = "insert into article(title, author, content, create_time) values(?, ?, ?, ?)";
         jdbcTemplate.update(sql, article.getTitle(), article.getAuthor(),
                 article.getContent(), article.getCreateTime());
     }
 
-    public void updateById(Article article) {
+    public void updateById(Article article, JdbcTemplate jdbcTemplate) {
         // 更新操作：使用update方法
         String sql = "update article set title = ?, author = ?, content = ?, create_time = ? where id = ?";
         jdbcTemplate.update(sql, article.getTitle(), article.getAuthor(),
                 article.getContent(), article.getCreateTime(), article.getId());
     }
 
-    public void deleteById(Long id) {
+    public void deleteById(Long id, JdbcTemplate jdbcTemplate) {
         // 删除操作：使用update方法
         String sql = "delete from article where id = ?";
         jdbcTemplate.update(sql, id);
     }
 
-    public Article findById(Long id) {
+    public Article findById(Long id, JdbcTemplate jdbcTemplate) {
         // 单行查询：使用queryForObject方法
         String sql = "select * from article where id = ?";
         return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Article.class), id);
     }
 
-    public List<Article> findAll() {
+    public List<Article> findAll(JdbcTemplate jdbcTemplate) {
         // 多行查询：使用query方法
         String sql = "select id, title, author, content, create_time from article";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Article.class));
     }
 
-    public Long count() {
+    public Long count(JdbcTemplate jdbcTemplate) {
         // 单值查询：使用queryForObject方法
         String sql = "select count(1) from article";
         return jdbcTemplate.queryForObject(sql, Long.class);
--- src/main/java/com/felix/learning/service/ArticleJdbcService.java
+++ src/main/java/com/felix/learning/service/ArticleJdbcService.java
@@ -2,40 +2,51 @@ package com.felix.learning.service;
 
 import com.felix.learning.dao.ArticleJdbcDao;
 import com.felix.learning.model.Article;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class ArticleJdbcService {
     @Resource
     private ArticleJdbcDao articleJdbcDao;
+    @Resource
+    private JdbcTemplate primaryJdbcTemplate;
+    @Resource
+    private JdbcTemplate secondaryJdbcTemplate;
 
+    @Transactional
     public void saveArticle(Article article) {
-        articleJdbcDao.save(article);
+        article.setCreateTime(LocalDateTime.now());
+        articleJdbcDao.save(article, primaryJdbcTemplate);
+        articleJdbcDao.save(article, secondaryJdbcTemplate);
+        int a = 10 / 0;
     }
 
     @Transactional
     public void updateArticle(Article article) {
-        articleJdbcDao.updateById(article);
-        int a = 10 / 0;
+        article.setCreateTime(LocalDateTime.now());
+        articleJdbcDao.updateById(article, primaryJdbcTemplate);
+        articleJdbcDao.updateById(article, secondaryJdbcTemplate);
     }
 
     public void deleteArticle(Long id) {
-        articleJdbcDao.deleteById(id);
+        articleJdbcDao.deleteById(id, primaryJdbcTemplate);
     }
 
     public Article getArticle(Long id) {
-        return articleJdbcDao.findById(id);
+        return articleJdbcDao.findById(id, primaryJdbcTemplate);
     }
 
     public List<Article> getAllArticle() {
-        return articleJdbcDao.findAll();
+        return articleJdbcDao.findAll(primaryJdbcTemplate);
     }
 
     public Long countOfArticle() {
-        return articleJdbcDao.count();
+        return articleJdbcDao.count(primaryJdbcTemplate);
     }
 }
